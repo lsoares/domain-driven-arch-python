91357483436328d1d0bb050be6709960bedb54ce
Luís Soares <lsoares@gmail.com>
2021-11-22 22:52:25 +0000

    create a user in memory

--- test/test_hello.py
+++ test/test_hello.py
@@ -3,10 +3,23 @@ from starlette.testclient import TestClient
 from main import app
 
 
-def test_hello_world():
+def test_create_a_user():
     client = TestClient(app)
 
-    response = client.get("/")
+    response = _create_user(client, "luis.s@gmail.com", "Luís Soares", "password")
 
-    assert response.status_code == 200
-    assert response.json() == {"hello": "dear"}
+    assert response.status_code == 201
+    assert _list_users(client).json() == [
+        {"name": "Luís Soares", "email": "luis.s@gmail.com"}
+    ]
+
+
+def _create_user(client, email, name, password):
+    return client.post(
+        url="/users",
+        json={"email": email, "name": name, "password": password}
+    )
+
+
+def _list_users(client):
+    return client.get(url="/users")
